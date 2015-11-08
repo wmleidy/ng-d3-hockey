825f7837d648adb2b87dd2a85e4d9a9cf689ccc3
Bill Leidy <wmleidy@gmail.com>
2015-11-08 15:03:36 -0600

    Fix issue with precision and scale

--- db/migrate/20151108052123_players.rb
+++ db/migrate/20151108052123_players.rb
@@ -10,20 +10,20 @@ class Players < ActiveRecord::Migration
   		t.integer :toi
   		t.integer :cf
   		t.integer :ca
-  		t.decimal :cf60, precision: 3
-  		t.decimal :ca60, precision: 3
-  		t.decimal :cf_per, precision: 3
-  		t.decimal :tmcf60, precision: 3
-  		t.decimal :tmca60, precision: 3
-  		t.decimal :tmcf_per, precision: 3
-  		t.decimal :oppcf60, precision: 3
-  		t.decimal :oppca60, precision: 3
-  		t.decimal :oppcf_per, precision: 3
-  		t.decimal :cf60reltm, precision: 3
-  		t.decimal :ca60reltm, precision: 3
-  		t.decimal :cf_per_reltm, precision: 3
-  		t.decimal :csh_per, precision: 3
-  		t.decimal :csv_per, precision: 3
+  		t.decimal :cf60, precision: 4, scale: 1
+  		t.decimal :ca60, precision: 4, scale: 1
+  		t.decimal :cf_per, precision: 4, scale: 1
+  		t.decimal :tmcf60, precision: 4, scale: 1
+  		t.decimal :tmca60, precision: 4, scale: 1
+  		t.decimal :tmcf_per, precision: 4, scale: 1
+  		t.decimal :oppcf60, precision: 4, scale: 1
+  		t.decimal :oppca60, precision: 4, scale: 1
+  		t.decimal :oppcf_per, precision: 4, scale: 1
+  		t.decimal :cf60reltm, precision: 4, scale: 2
+  		t.decimal :ca60reltm, precision: 4, scale: 2
+  		t.decimal :cf_per_reltm, precision: 4, scale: 1
+  		t.decimal :csh_per, precision: 4, scale: 1
+  		t.decimal :csv_per, precision: 4, scale: 1
   		t.decimal :cpdo, precision: 4, scale: 1
   		t.integer :totfo
   		t.integer :nzfo
@@ -32,27 +32,27 @@ class Players < ActiveRecord::Migration
   		t.integer :igoals
   		t.integer	:iassists
   		t.integer :ipoints
-  		t.decimal :ishot_per, precision: 3
-  		t.decimal :igoals60, precision: 3
-  		t.decimal :iassists60, precision: 3
-  		t.decimal :ipoints60, precision: 3
-  		t.decimal :ipp, precision: 3
-  		t.decimal :igp, precision: 3
-  		t.decimal :iap, precision: 3
+  		t.decimal :ishot_per, precision: 4, scale: 1
+  		t.decimal :igoals60, precision: 4, scale: 2
+  		t.decimal :iassists60, precision: 4, scale: 2
+  		t.decimal :ipoints60, precision: 4, scale: 2
+  		t.decimal :ipp, precision: 4, scale: 1
+  		t.decimal :igp, precision: 4, scale: 1
+  		t.decimal :iap, precision: 4, scale: 1
   		t.integer :gf
   		t.integer :ga
-  		t.decimal :gf60, precision: 3
-  		t.decimal :ga60, precision: 3
-  		t.decimal :gf_per, precision: 3
-  		t.decimal :tmgf60, precision: 3
-  		t.decimal :tmga60, precision: 3
-  		t.decimal :tmgf_per, precision: 3
-  		t.decimal :oppgf60, precision: 3
-  		t.decimal :oppga60, precision: 3
-  		t.decimal :oppgf_per, precision: 3
-  		t.decimal :gf60reltm, precision: 3
-  		t.decimal :ga60reltm, precision: 3
-  		t.decimal :gf_per_reltm, precision: 3
+  		t.decimal :gf60, precision: 4, scale: 2
+  		t.decimal :ga60, precision: 4, scale: 2
+  		t.decimal :gf_per, precision: 4, scale: 1
+  		t.decimal :tmgf60, precision: 4, scale: 2
+  		t.decimal :tmga60, precision: 4, scale: 2
+  		t.decimal :tmgf_per, precision: 4, scale: 1
+  		t.decimal :oppgf60, precision: 4, scale: 2
+  		t.decimal :oppga60, precision: 4, scale: 2
+  		t.decimal :oppgf_per, precision: 4, scale: 1
+  		t.decimal :gf60reltm, precision: 4, scale: 2
+  		t.decimal :ga60reltm, precision: 4, scale: 2
+  		t.decimal :gf_per_reltm, precision: 4, scale: 1
   	end
   end
 end
